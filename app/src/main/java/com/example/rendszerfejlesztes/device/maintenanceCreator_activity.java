47b3afcd8389549b25e957490fa10384052d7b1c
doraviktoria-kiss <74766378+doraviktoria-kiss@users.noreply.github.com>
2022-04-23 21:01:09 +0200

    maintenance get deviceID from database, not manually

--- app/src/main/java/com/example/rendszerfejlesztes/device/maintenanceCreator_activity.java
+++ app/src/main/java/com/example/rendszerfejlesztes/device/maintenanceCreator_activity.java
@@ -5,37 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.rendszerfejlesztes.R;
+import com.example.rendszerfejlesztes.models.DeviceModel;
 import com.example.rendszerfejlesztes.services.categoryServices;
+import com.example.rendszerfejlesztes.services.deviceServices;
 import com.example.rendszerfejlesztes.services.maintenanceServices;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class maintenanceCreator_activity extends AppCompatActivity {
 
-    EditText deviceID, date, instructions;
+    EditText date, instructions;
     Button add, back;
     maintenanceServices mServices = new maintenanceServices();
+    Spinner spin;
+    List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintenance_creator);
 
-        deviceID = findViewById(R.id.deviceID_et);
         date = findViewById(R.id.date_et);
         instructions = findViewById(R.id.instructions_et);
         add = findViewById(R.id.add_b);
         back = findViewById(R.id.back_b);
+        spin = findViewById(R.id.spin_devID);
 
         getSupportActionBar().hide();
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mServices.addCategory(deviceID.getText().toString(), date.getText().toString(), instructions.getText().toString(), new categoryServices.VolleyResponsePOSTListener() {
+                mServices.addCategory(spin.getSelectedItem().toString(), date.getText().toString(), instructions.getText().toString(), new categoryServices.VolleyResponsePOSTListener() {
                     @Override
                     public void onResponse(String message) {
                         Toast.makeText(maintenanceCreator_activity.this, message, Toast.LENGTH_SHORT).show();
@@ -53,6 +63,28 @@ public class maintenanceCreator_activity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent back = new Intent(getApplicationContext(), deviceManager_activity.class);
                 startActivity(back);
+            }
+        });
+
+        deviceServices.getDevices(new deviceServices.VolleyResponseGETDEVICESListener() {
+            @Override
+            public void onError(String message) {
+                Toast.makeText(maintenanceCreator_activity.this, message , Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onResponse(ArrayList<DeviceModel> deviceModels) {
+                ArrayAdapter arrayAdapter = new ArrayAdapter(maintenanceCreator_activity.this, android.R.layout.simple_list_item_1, deviceModels);
+                for(int i=0;i<deviceModels.size();i++) {
+                    list.add(Integer.toString(deviceModels.get(i).id));
+                }
+                String[] deviceIDs = new String[list.size()];
+                for (int i = 0; i < list.size(); i++) {
+                    deviceIDs[i] = list.get(i);
+                }
+                ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(
+                        maintenanceCreator_activity.this, android.R.layout.simple_spinner_item, deviceIDs);
+                spinnerArrayAdapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+                spin.setAdapter(spinnerArrayAdapter);
             }
         });
     }
