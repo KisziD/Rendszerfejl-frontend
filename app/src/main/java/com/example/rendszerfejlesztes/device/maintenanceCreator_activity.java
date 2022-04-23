ca3c22f9c5e3f8c875ff511f9c84fd333c1d622b
doraviktoria-kiss <74766378+doraviktoria-kiss@users.noreply.github.com>
2022-04-23 19:28:21 +0200

    maintenance service

---
+++ app/src/main/java/com/example/rendszerfejlesztes/device/maintenanceCreator_activity.java
@@ -0,0 +1,14 @@
+package com.example.rendszerfejlesztes.device;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class maintenanceCreator_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintenance_creator);
+    }
+}
