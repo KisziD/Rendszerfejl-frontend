657f517696de25edb1ee23998a86fc318cdd1565
doraviktoria-kiss <74766378+doraviktoria-kiss@users.noreply.github.com>
2022-04-23 19:37:47 +0200

    add maintenance services

--- app/src/main/java/com/example/rendszerfejlesztes/device/maintenanceCreator_activity.java
+++ app/src/main/java/com/example/rendszerfejlesztes/device/maintenanceCreator_activity.java
@@ -2,13 +2,58 @@ package com.example.rendszerfejlesztes.device;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.rendszerfejlesztes.R;
+import com.example.rendszerfejlesztes.services.categoryServices;
+import com.example.rendszerfejlesztes.services.maintenanceServices;
 
 public class maintenanceCreator_activity extends AppCompatActivity {
+
+    EditText deviceID, date, instructions;
+    Button add, back;
+    maintenanceServices mServices = new maintenanceServices();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintenance_creator);
+
+        deviceID = findViewById(R.id.deviceID_et);
+        date = findViewById(R.id.date_et);
+        instructions = findViewById(R.id.instructions_et);
+        add = findViewById(R.id.add_b);
+        back = findViewById(R.id.back_b);
+
+        getSupportActionBar().hide();
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mServices.addCategory(deviceID.getText().toString(), date.getText().toString(), instructions.getText().toString(), new categoryServices.VolleyResponsePOSTListener() {
+                    @Override
+                    public void onResponse(String message) {
+                        Toast.makeText(maintenanceCreator_activity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(maintenanceCreator_activity.this, message, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back = new Intent(getApplicationContext(), deviceManager_activity.class);
+                startActivity(back);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/rendszerfejlesztes/services/maintenanceServices.java
+++ app/src/main/java/com/example/rendszerfejlesztes/services/maintenanceServices.java
@@ -1,2 +1,56 @@
-package com.example.rendszerfejlesztes.services;public class maintenanceServices {
+package com.example.rendszerfejlesztes.services;
+
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.rendszerfejlesztes.device.maintenanceCreator_activity;
+
+import java.util.List;
+
+public class maintenanceServices {
+
+    public interface VolleyResponsePOSTListener {
+        void onError(String message);
+        void onResponse(String message);
+    }
+
+    public interface VolleyResponseMainTListener {
+        void onError(String message);
+        void onResponse(List<String> ls);
+    }
+
+    public final static String MAINT_POST = "http://kisziftp.tplinkdns.com/api/Maintenance/add";
+    static Context context;
+
+    public void addCategory(String deviceID, String date, String justification, categoryServices.VolleyResponsePOSTListener volleyResponsePOSTListener) {
+
+        String post_url = MAINT_POST;
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("deviceID", deviceID);
+            postData.put("date", date);
+            postData.put("justification", justification);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, post_url, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                volleyResponsePOSTListener.onResponse("Maintenance added");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                volleyResponsePOSTListener.onError("Maintenance addition failed");
+            }
+        });
+        SingletonRequestQueue.getInstance(context).addToRequestQueue(request);
+    }
 }
