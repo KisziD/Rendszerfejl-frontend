57a3be368632901bcd963b8a3fa62d4b8b96a44e
doraviktoria-kiss <74766378+doraviktoria-kiss@users.noreply.github.com>
2022-05-07 21:10:08 +0200

    set states

---
+++ app/src/main/java/com/example/rendszerfejlesztes/interfaces/taskStateChanger_activity.java
@@ -0,0 +1,14 @@
+package com.example.rendszerfejlesztes.interfaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class taskStateChanger_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_state_changer);
+    }
+}
